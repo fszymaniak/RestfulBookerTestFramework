849bd7e18a19ba15a660536a9b3c7a3e17f6fb30
fszymaniak <szymaniak.filip@gmail.com>
2025-02-08 17:48:09 +0100

    test(performance): Add PerformanceTestInjectDeleteBooking.feature

---
+++ tests/RestfulBookerTestFramework.Tests.Performance/Features/InjectFeatures/PerformanceTestInjectDeleteBooking.feature
@@ -0,0 +1,15 @@
+﻿@PerformanceTest
+@Inject
+@DeleteBookingFeature
+@AuthorizeRequest
+Feature: Performance test inject of deleting booking
+
+Description:
+As RestfulBooker user
+I want to sent valid DELETE request with valid booking id to the /booking endpoint
+So that I will be able to sucessfully create delete entity (201 Created Status Code) and test performance of this endpoint
+
+    Scenario: Performance Inject Delete Booking Endpoint 
+        Given Prerequisite: API is running
+        And a new valid booking request is created
+        When run inject delete performance scenario: 'Create and then delete booking (inject)'  with Rate: 5, Interval in seconds: 1 and During in seconds: 5
--- tests/RestfulBookerTestFramework.Tests.Performance/StepDefinitions/PerformanceInjectLoadSimulationSteps.cs
+++ tests/RestfulBookerTestFramework.Tests.Performance/StepDefinitions/PerformanceInjectLoadSimulationSteps.cs
@@ -17,25 +17,55 @@ public class PerformanceInjectLoadSimulationSteps(IPerformanceHelper performance
 {
     private static readonly HttpClient HttpClient = new();
     private List<int> _bookingIdsList = new();
-    
+
     [When("run inject performance scenario: '(.*)' for '(.*)' method and '(.*)' endpoint with Rate: (.*), Interval in seconds: (.*) and During in seconds: (.*)")]
-    public void RunInjectPerformanceScenario(string scenarioName, string method, string endpoint, int rate, int interval, int during)
-    {                                                                                           
+    public void RunInjectPerformanceScenario(string scenarioName, string method, string endpoint, int rate,
+        int interval, int during)
+    {
         var scenario = Scenario.Create(scenarioName, async context =>
             {
                 var request = performanceHelper.CreatePerformanceRequest(method, endpoint);
 
                 var response = await Http.Send(HttpClient, request);
-                
+
                 _bookingIdsList.SetCreatedBookingIds(method, endpoint, response, scenarioContext);
 
                 return response;
             })
             .WithoutWarmUp()
             .WithLoadSimulations(
-                Simulation.Inject(rate: rate, 
-                    interval: TimeSpan.FromSeconds(interval),
-                    during: TimeSpan.FromSeconds(during))
+                Simulation.Inject(rate,
+                    TimeSpan.FromSeconds(interval),
+                    TimeSpan.FromSeconds(during))
+            );
+
+        NBomberRunner
+            .RegisterScenarios(scenario)
+            .Run();
+    }
+
+    [When("run inject delete performance scenario: '(.*)'  with Rate: (.*), Interval in seconds: (.*) and During in seconds: (.*)")]
+    public void RunInjectDeletePerformanceScenario(string scenarioName, int rate, int interval, int during)
+    {
+        var scenario = Scenario.Create(scenarioName, async context =>
+            {
+                var createBookingRequest = performanceHelper.CreatePerformanceRequest("POST", Endpoints.BookingEndpoint);
+
+                var createBookingResponse = await Http.Send(HttpClient, createBookingRequest);
+                
+                var booking = createBookingResponse.Deserialize<BookingResponse>();
+                
+                var deleteBookingRequest = performanceHelper.CreatePerformanceRequest("DELETE", Endpoints.DeleteEndpoint, booking.BookingId);
+
+                var deleteBookingResponse = await Http.Send(HttpClient, deleteBookingRequest);
+
+                return deleteBookingResponse;
+            })
+            .WithoutWarmUp()
+            .WithLoadSimulations(
+                Simulation.Inject(rate,
+                    TimeSpan.FromSeconds(interval),
+                    TimeSpan.FromSeconds(during))
             );
 
         NBomberRunner
