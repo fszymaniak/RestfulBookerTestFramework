65cd3d0446043b37eb36b52640b457b80797441a
fszymaniak <szymaniak.filip@gmail.com>
2025-02-09 12:33:05 +0100

    test(performance): add PerformanceTestInjectRandomDeleteBooking.feature

---
+++ tests/RestfulBookerTestFramework.Tests.Performance/Features/InjectRandomFeatures/PerformanceTestInjectRandomDeleteBooking.feature
@@ -0,0 +1,15 @@
+﻿@PerformanceTest
+@InjectRandom
+@DeleteBookingFeature
+@AuthorizeRequest
+Feature: Performance test inject random of deleting booking
+
+Description:
+As RestfulBooker user
+I want to sent valid DELETE request with valid booking id to the /booking endpoint
+So that I will be able to sucessfully create delete entity (201 Created Status Code) and test performance of this endpoint
+
+    Scenario: Performance Inject Random Delete Booking Endpoint 
+        Given Prerequisite: API is running
+        And a new valid booking request is created
+        When run inject random delete performance scenario: 'Create and then delete booking (inject random)' with MinRate: 5, MaxRate: 10, Interval in seconds: 1 and During in seconds: 5
--- tests/RestfulBookerTestFramework.Tests.Performance/StepDefinitions/PerformanceInjectRandomLoadSimulationSteps.cs
+++ tests/RestfulBookerTestFramework.Tests.Performance/StepDefinitions/PerformanceInjectRandomLoadSimulationSteps.cs
@@ -4,6 +4,9 @@ using System.Net.Http;
 using NBomber.CSharp;
 using NBomber.Http.CSharp;
 using Reqnroll;
+using RestfulBookerTestFramework.Tests.Commons.Constants;
+using RestfulBookerTestFramework.Tests.Commons.Extensions;
+using RestfulBookerTestFramework.Tests.Commons.Payloads.Responses;
 using RestfulBookerTestFramework.Tests.Performance.Extensions;
 using RestfulBookerTestFramework.Tests.Performance.Helpers;
 
@@ -27,6 +30,37 @@ public class PerformanceInjectRandomLoadSimulationSteps(IPerformanceHelper perfo
                 _bookingIdsList.SetCreatedBookingIds(method, endpoint, response, scenarioContext);
 
                 return response;
+            })
+            .WithoutWarmUp()
+            .WithLoadSimulations(
+                Simulation.InjectRandom(
+                    minRate: minRate, 
+                    maxRate: maxRate, 
+                    interval: TimeSpan.FromSeconds(interval),
+                    during: TimeSpan.FromSeconds(during))
+            );
+
+        NBomberRunner
+            .RegisterScenarios(scenario)
+            .Run();
+    }
+    
+    [When("run inject random delete performance scenario: '(.*)' with MinRate: (.*), MaxRate: (.*), Interval in seconds: (.*) and During in seconds: (.*)")]
+    public void RunInjectDeletePerformanceScenario(string scenarioName, int minRate, int maxRate, int interval, int during)
+    {
+        var scenario = Scenario.Create(scenarioName, async context =>
+            {
+                var createBookingRequest = performanceHelper.CreatePerformanceRequest("POST", Endpoints.BookingEndpoint);
+
+                var createBookingResponse = await Http.Send(HttpClient, createBookingRequest);
+                
+                var booking = createBookingResponse.Deserialize<BookingResponse>();
+                
+                var deleteBookingRequest = performanceHelper.CreatePerformanceRequest("DELETE", Endpoints.DeleteEndpoint, booking.BookingId);
+
+                var deleteBookingResponse = await Http.Send(HttpClient, deleteBookingRequest);
+
+                return deleteBookingResponse;
             })
             .WithoutWarmUp()
             .WithLoadSimulations(
